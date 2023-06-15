5d8ac6956afe6e49c8fabd22078f71a451038e92
R4Tobi <R4Tobi@users.noreply.github.com>
2023-06-15 11:41:36 +0200

    init project

---
+++ src/components/Main.vue
@@ -0,0 +1,22 @@
+<template>
+ <div class="main">
+
+ </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Main',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.main{
+  
+}
+</style>
